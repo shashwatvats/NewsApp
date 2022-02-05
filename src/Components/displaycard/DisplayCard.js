c9fd2c416ad3ae171e7db4b9fcab8c1f25a7cc10
Shashwat Vats <shashwat.vats@globallogic.com>
2022-02-05 20:47:06 +0530

    added perfect scrollbar

--- src/Components/displaycard/DisplayCard.js
+++ src/Components/displaycard/DisplayCard.js
@@ -1,24 +1,28 @@
 import React from "react";
+import "react-perfect-scrollbar/dist/css/styles.css";
+import PerfectScrollbar from "react-perfect-scrollbar";
 
 function DisplayCard(props) {
   return (
     <div>
       <div
         style={{ maxHeight: "350px", minWidth: "200px" }}
-        className="card mb-2 overflow-auto"
+        className="card mb-2"
         data-testid="card"
       >
-        <img
-          className="card-img-top"
-          src={props.url}
-          style={{ maxHeight: "150px" }}
-          alt="Image Not Found!"
-          data-testid="img"
-        />
-        <div className="card-body">
-          <h5 className="card-title">{props.title}</h5>
-          <p className="card-text">{props.description}</p>
-        </div>
+        <PerfectScrollbar>
+          <img
+            className="card-img-top"
+            src={props.url}
+            style={{ maxHeight: "150px" }}
+            alt="Image Not Found!"
+            data-testid="img"
+          />
+          <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.description}</p>
+          </div>
+        </PerfectScrollbar>
       </div>
     </div>
   );
